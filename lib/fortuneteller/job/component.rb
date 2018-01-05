6e39f849d83f82ec3a8fcac38f279c7b7d5b35b5
Colin Sidoti <colin@chrtwt.org>
2018-01-05 03:28:24 -0500

    Fix Wage Growth Test, remove old code

--- lib/fortuneteller/job/component.rb
+++ lib/fortuneteller/job/component.rb
@@ -13,50 +13,6 @@ module FortuneTeller
           generators[year] = FortuneTeller::Job::Generator.new(data: data, holder: holder, year: year)
         end
       end
-
-      private
-
-      def gen_transforms(from:, to:, simulator:)
-        # TODO: Update so it responds to changes within the year
-        fields = gen_transform_fields(plan.to_reader.on(from), from)
-        transforms = []
-        transforms.push gen_transform(from, fields) if from.day == 1
-        current = next_month(from)
-
-        while current < to
-          transforms.push gen_transform(current, fields)
-          current = next_month(current)
-        end
-
-        transforms
-      end
-      
-      def gen_transform(date, fields)
-        self.class.parent::Transform.new(date: date, holder: @holder, **fields)
-      end
-
-      def gen_transform_fields(day_plan, from)
-        wages = day_plan.base / 12.0
-        account_credits = {}
-        income = {
-          wages: wages,
-          saved: Utils::InflatingInt.zero,
-          matched: Utils::InflatingInt.zero,
-          pay_period: :monthly
-        }
-
-        day_plan.savings_plans.each do |p|
-          saved = wages * (p[:percent] / 100.0)
-          income[:saved] = saved + income[:saved]
-
-          matched = wages * (p[:match] / 100.0)
-          income[:matched] = matched + income[:matched]
-
-          account_credits[p[:account].key] = { p[:holding] => saved + matched }
-        end
-
-        { account_credits: account_credits, income: income }
-      end
     end
   end
 end
--- test/job_test.rb
+++ test/job_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class JobTest < MiniTest::Test
   def test_wage_growth
     sim = MiniTest::Mock.new
+    sim.expect(:start_year, Date.today.year)
+    sim.expect(:end_year, Date.today.year+1)
+    sim.expect(:beginning, Date.today)
+
     growth_rates = FortuneTeller::GrowthRateSet.new(
       { wage_growth: [1.02] }, start_year: Date.today.year
     )
@@ -11,11 +15,9 @@ class JobTest < MiniTest::Test
       plan.beginning.set(base: 120_000_00)
     end
 
-    transforms = component.bounded_gen_transforms(
-      from: Date.today + 1.year,
-      to: Date.today + 2.years,
-      simulator: sim
-    )
+    component.build_generators(sim)
+
+    transforms = component.generators[(Date.today.year+1)].gen_transforms(simulator: sim)
 
     assert_equal 10200_00, transforms.first.income[:wages].on(Date.today + 1.year, growth_rates: growth_rates)
 
